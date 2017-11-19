5544c6bf2a1a1b71ab1582bfba9ba9091ecc43b6
Kyle Magnuson <kyle@collectiveidea.com>
2017-11-18 22:11:41 -0600

    Setup makefile

--- handler.py
+++ handler.py
@@ -1,2 +1,6 @@
-def hello_world()
-    print("Hello World!")
+"""The function to run on the Lambda"""
+def hello_world(_event, _context):
+    """
+    Everyone's favorite one liner.
+    """
+    print "Hello World!"
